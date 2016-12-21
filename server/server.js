4dea4ab7c716c7083ac66897c338ee734ed29d45
kiran <kiran@neostack.com>
2016-12-21 15:45:41 +0530

    Added webpack-dev-server setup in server

--- server/server.js
+++ server/server.js
@@ -1,23 +1,40 @@
-var express = require('express');
-var app = express();
-var api = require('./api/api');
+var express = require('express')
+var app = express()
+var api = require('./api/api')
 var err = require('./middleware/err')
 var path = require('path')
+var morgan = require('morgan')
+
+var env = process.env.NODE_ENV || 'development'
+
+if (env === 'development') {
+  var webpack = require('webpack')
+  var config = require('../webpack.config')
+  var compiler = webpack(config)
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }))
+  app.use(require('webpack-hot-middleware')(compiler))
+  app.use('/public', express.static(path.join(__dirname, '../public')))
+  app.use(express.static(path.join(__dirname, '../dist')))
+  app.use(morgan('combined'))
+}
 
 // setup the app middlware
-require('./middleware/appMiddleware')(app);
+require('./middleware/appMiddleware')(app)
 
 // setup the api
-app.use('/api', api);
+app.use('/api', api)
 
 // set up global error handling
 // // pattern 1
-// app.use(err);
+// app.use(err)
 // pattern 2
-app.use(err());
+app.use(err())
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
-module.exports = app;
+module.exports = app
