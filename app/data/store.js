030fd5b5c28c17aa6a6c89a1b022aba98b5d1838
kiran <kiran@neostack.com>
2016-12-21 17:08:23 +0530

    redux thunk middleware setup

--- app/data/store.js
+++ app/data/store.js
@@ -1,11 +1,15 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { reducer as formReducer } from 'redux-form'
+import thunk from 'redux-thunk'
 
 const reducers = {
   form: formReducer
 }
 
 const reducer = combineReducers(reducers)
-const store = createStore(reducer)
+const store = createStore(
+                reducer,
+                applyMiddleware(thunk)
+              )
 
 export default store
