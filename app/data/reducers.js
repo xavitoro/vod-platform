8314d6c033ca4211515b4ed4a8a2b7cbd470393a
kiran <kiran@neostack.com>
2016-12-24 23:07:09 +0530

    added authors reducer

--- app/data/reducers.js
+++ app/data/reducers.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import ingredients from './ingredients'
 import learningPaths from './learningPaths'
+import authors from './authors'
 
 const reducers = {
   ingredients,
   learningPaths,
+  authors,
   form: formReducer
 }
 
