f2fe23c1f0ab78a90b7042d1f574ad3086caa2de
ASJ <aserrajaumot@gmail.com>
2017-01-04 13:54:15 +0100

    Improving IngredientList

--- app/components/RecipeDisplay/IngredientList.js
+++ app/components/RecipeDisplay/IngredientList.js
@@ -9,13 +9,15 @@ function IngredientList (props) {
           .map((ingredient, index) => {
             var {name, quantity, unit} = ingredient
             return (
-              <div key={index}>
-                {`Ingredient ${index + 1}: ${quantity} ${name} ${unit}`}
+              <div key={index} className = 'ingredient'>
+                <div className='ingredient-quantity'> {`Qty: ${quantity}`}</div>
+                <div className='ingredient-name'> {`Name: ${name}`}</div>
+                <div className='ingredient-unit'> {`Unit: ${unit}`}</div>
               </div>
             )
           })}
-          </div>
-        </div>
+      </div>
+    </div>
   )
 }
 IngredientList.propTypes = {
