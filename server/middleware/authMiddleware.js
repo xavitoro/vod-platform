11a4921e6e09dafeb29a1c4a57936172ae7ff2af
kiran <kiran@neostack.com>
2017-01-08 23:20:01 +0530

    added checkAdmin method

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,28 +2,37 @@ const jwt = require('jsonwebtoken');
 
 const secretKey = process.env.SECRET_KEY || 'vodvod';
 
-module.exports = {
-  checkUser: function (req, res, next) {
-    jwt.verify(req.headers.vodauth, secretKey, function(err, decoded) {
-      if (err) next(err);
-
-      var user = decoded._doc;
-
-      if (user._id) {
-        req.currentUser = user;
-        next();
-      } else {
-        next(new Error('There is an issue getting user'));
-      }
-    });
-  },
-  checkAdmin: function (req, res, next) {
-    // var user = req.currentUser;
-    // if (user.isAdmin) {
-    //   next();
-    // } else {
-    //   next(new Error('User is not an admin'));
-    // }
-    next();
+export function checkAdmin(req, res, next) {
+  const user = req.user
+  if (user && user.isAdmin) {
+    next()
+  } else {
+    res.status(401)
   }
 }
+
+// module.exports = {
+//   checkUser: function (req, res, next) {
+//     jwt.verify(req.headers.vodauth, secretKey, function(err, decoded) {
+//       if (err) next(err);
+//
+//       var user = decoded._doc;
+//
+//       if (user._id) {
+//         req.currentUser = user;
+//         next();
+//       } else {
+//         next(new Error('There is an issue getting user'));
+//       }
+//     });
+//   },
+//   checkAdmin: function (req, res, next) {
+//     // var user = req.currentUser;
+//     // if (user.isAdmin) {
+//     //   next();
+//     // } else {
+//     //   next(new Error('User is not an admin'));
+//     // }
+//     next();
+//   }
+// }
