a4fb82a02643d412ea24633cd5b2ecc9c491f117
ASJ <aserrajaumot@gmail.com>
2017-01-18 18:20:20 +0100

    Refactoring html&css: RecipeComponent

--- app/components/RecipeDisplay/OverviewDisplay/RecipeComponent.js
+++ app/components/RecipeDisplay/OverviewDisplay/RecipeComponent.js
@@ -2,54 +2,34 @@ import React, { PropTypes } from 'react'
 
 import Moment from 'react-moment'
   // const Moment=require('react-moment').default
-import { StyleSheet, css } from 'aphrodite'
+// import { StyleSheet, css } from 'aphrodite'
 
 function RecipeComponent(props) {
   return (
-    <div className='card-recipe col-md-12'>
+    <div className='recipe-component'>
       <div className='recipe-video-preview-holder'>
-      <img className='recipe-video-preview-detailed'
-        src={
-          `/public/img/recipes/${props.videoThumbnail}`
-        }
-      />
+        <img className='recipe-image' src={`/public/img/recipes/${props.videoThumbnail}`}/>
       </div>
-      <div className='card-recipe-inner'>
-        <h3 className='recipe-title'> {
-            props.title
-          }
-        </h3>
+      <div className='card-recipe'>
+        <h3>{props.title}</h3>
       </div>
       <div className='recipe-author'>
-        <img className={
-            `img-responsive recipe-author-thumbnail ${css(styles.author)}`
-          }
-          src={
-            `/public/img/recipes/${props.author.thumbnail}`
-          }
-        />
-        <h4 className='recipe-author-name'> {
-            props.author.name
-          }
-        </h4>
+        <img className='recipe-author-avatar round-img' src={`/public/img/recipes/${props.author.thumbnail}`} />
+        <span className='recipe-author-name'>{props.author.name}</span>
       </div>
-      <div className={
-          `card-content ${css(styles.cardcontent)}`
-        }>
-        <div className={
-            `creation-date ${css(styles.date)}`
-          }>
-          Date:
-            <Moment unix format='MM/YYYY'>{props.created}</Moment>
-          </div>
-        <div className="recipe categories">
-          Categories: {
-            props.categories.join(' | ')
-          }
+      <div className='card-content'>
+        {/* <h5>Date:</h5>
+        <Moment unix format='MM/YYYY'>{props.created}</Moment> */}
+        <div className='recipe-categories'>
+          {/* <h5>Categories: </h5> */}
+          <ul>
+            {props.categories
+              .map((category) => <li key={category}>{category}</li>)
+            }
+          </ul>
         </div>
-        <div className="recipe-description"> {
-            props.description
-          }
+        <div className='recipe-description'>
+          <h5>{props.description}</h5>
         </div>
       </div>
     </div>
@@ -70,20 +50,3 @@ RecipeComponent.propTypes={
 }
 
 export default RecipeComponent
-
-
-const styles=StyleSheet.create({
-  author: {
-    width: 30,
-    height: 30,
-    borderRadius: 50,
-    float: 'left',
-    'margin-right': 10,
-  },
-  cardcontent: {
-    float: 'left',
-  },
-  date: {
-    float: 'right',
-  }
-})
