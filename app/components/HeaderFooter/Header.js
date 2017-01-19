c4db090b5eef414fdee7e116695527b7a38fe65d
ASJ <aserrajaumot@gmail.com>
2017-01-19 17:58:39 +0100

    Adding a sub-section in the header to get the users mail

--- app/components/HeaderFooter/Header.js
+++ app/components/HeaderFooter/Header.js
@@ -14,6 +14,14 @@ const Header = React.createClass({
             <Link to='/recipe/create' href='/'>Create Recipe</Link>
             <Link to='/register' href='/' id ='register'>Register</Link>
           </nav>
+          <div className='secondary-header'>
+            <h1>Learn with chefs</h1>
+            <p>Attend to our live MasterClasses or watch the recorded video</p>
+            <form className='form-email'>
+              <input type='email' value='' placeholder='I want to cook! Take my email!' id='email-value' required=''/>
+              <input type='submit' className='btn' id='submit-email' value='Get started for FREE'/>
+            </form>
+          </div>
         </div>
       </header>
     )
