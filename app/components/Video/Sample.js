62123e74ed537cadce2eec73d703ab8d92037756
kiran <kiran@neostack.com>
2017-01-27 16:17:56 +0530

    sample video with jwplayer

---
+++ app/components/Video/Sample.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import JWPlayer from 'react-jwplayer'
+
+export default class SampleVideo extends React.Component {
+  render() {
+    return (
+      <div>
+        <JWPlayer videoId="y8Bp51TG" />,
+      </div>
+    )
+  }
+}
